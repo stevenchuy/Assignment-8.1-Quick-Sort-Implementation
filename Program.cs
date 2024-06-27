e4a4116143e8a597468e41782016d4c0765ca2e5
steve <steve@DESKTOP-8OHDCI7>
2024-06-27 14:22:18 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Assignment_8._1_Quick_Sort_Implementation
+{
+    internal class Program
+    {
+        // Driver Code
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 7, 9, 10 };
+            int N = arr.Length;
+
+            // Function call
+            JMJ letsGo = new JMJ();
+            letsGo.quickSort(arr, 0, N - 1);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < N; i++)
+                Console.Write(arr[i] + " ");
+        }
+    }
+    class JMJ
+    {
+        // The main function that implements QuickSort
+        // arr[] --> Array to be sorted,
+        // low --> Starting index,
+        // high --> Ending index
+        public void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                // pi is partitioning index, arr[p]
+                // is now at right place
+                int pi = partition(arr, low, high);
+
+                // Separately sort elements before
+                // and after partition index
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+        // This function takes last element as pivot,
+        // places the pivot element at its correct position
+        // in sorted array, and places all smaller to left
+        // of pivot and all greater elements to right of pivot
+        static int partition(int[] arr, int low, int high)
+        {
+            // Choosing the pivot
+            int pivot = arr[high];
+
+            // Index of smaller element and indicates
+            // the right position of pivot found so far
+            int i = (low - 1);
+
+            for (int j = low; j <= high - 1; j++)
+            {
+
+                // If current element is smaller than the pivot
+                if (arr[j] < pivot)
+                {
+
+                    // Increment index of smaller element
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            return (i + 1);
+        }
+        // A utility function to swap two elements
+        static void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        } 
+    }
+}
